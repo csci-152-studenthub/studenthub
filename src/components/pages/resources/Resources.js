ca36afbb327d107f370902734d9b87832da38d67
Nina <swaavayyy@mail.fresnostate.edu>
2019-02-19 17:06:08 -0800

    added skeleton components

---
+++ src/components/pages/resources/Resources.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Resources extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default Resources
---
+++ src/components/pages/studygroup/Studygroup.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Studygroup extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default Studygroup
