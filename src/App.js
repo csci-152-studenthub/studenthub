051825f65da5a73c3e8c161698847335686e4f3b
Erick Castro <ecast96@mail.fresnostate.edu>
2019-03-03 18:57:14 -0800

    Adding files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import React, { Component } from 'react';
+import {message, Layout, Col, Row, Divider} from 'antd';
+import Form from './SignIn';
+import SignUp from './SignUp';
+import logo from './assets/logo.jpg';
 import './App.css';
 
+const {
+  Header, Footer, Sider, Content,
+} = Layout;
+
+const success = () => {
+  message.loading('Action in progress..', 2.5)
+    .then(() => message.success('Loading finished', 2.5));
+  };
+
 class App extends Component {
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <p>
-            HELLO WORLD
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Row type="flex" align="middle"  justify="space-around" style={{ paddingTop: 100}}>
+          <Col span={6} ><img src={logo} alt="Logo" style={{ height: 400, paddingLeft: 200, paddingRight: 10}}/></Col>
+          <Col span={1}><Divider type="vertical" style={{height: 300, left: 125}} /></Col>
+          <Col span={10}><Form /></Col>
+        </Row>
       </div>
     );
   }
---
+++ studenthub-api/get.js
@@ -0,0 +1,25 @@
+import * as dynamoDbLib from "./libs/dynamodb-lib";
+import { success, failure } from "./libs/response-lib";
+
+export async function main(event, context) {
+  const params = {
+    TableName: "posts",
+    Key: {
+      userid: event.requestContext.identity.cognitoIdentityId,
+      postid: event.pathParameters.id
+    }
+  };
+
+  try {
+    const result = await dynamoDbLib.call("get", params);
+    if (result.Item) {
+      // Return the retrieved item
+      return success(result.Item);
+    } else {
+      return failure({ status: false, error: "Item not found." });
+    }
+  } catch (e) {
+    console.log(e);
+    return failure({ status: false });
+  }
+}
