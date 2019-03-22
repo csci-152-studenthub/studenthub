94f68cfc5182d6209b9d6b4a243589af605aa091
b6i0l5l <38479828+b6i0l5l@users.noreply.github.com>
2019-03-21 18:17:26 -0700

    put file into dashboard
    dashboard css and js

--- src/components/pages/dashboard/Dashboard.js
+++ src/components/pages/dashboard/Dashboard.js
@@ -1,13 +1,43 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+// import { Layout } from 'antd';
+// import { Button } from 'antd';
+// import { List, Icon, Tag } from 'antd';
+
 import './Dashboard.css';
-export class Dashboard extends Component {
+
+export class CardContainer extends Component {
+
   render() {
     return (
       <div>
-       
+        <div class="grid-head">
+          <div>Hello!</div>
+        </div>
+        <div class="grid-container">
+          <div><a href='linkhref.html' id='Feeds'>Feeds</a>
+          
+
+          </div>
+          <div>Resources</div>
+          <div>StudyGroup</div>
+        </div>
+        <div class="grid-bottom">
+          <div></div>
+        </div>
+
+
+
+
+
+
+
+
+
       </div>
+
     )
+    
   }
 }
 
-export default Dashboard
+export default CardContainer
