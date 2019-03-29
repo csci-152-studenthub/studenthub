56811862df0fd820cc2383fcf54e7844dc4bcf08
leng03 <leng03@mail.fresnostate.edu>
2019-03-28 20:10:46 -0700

    updated
    add images

--- src/components/pages/resources/resComp/Cards.js
+++ src/components/pages/resources/resComp/Cards.js
@@ -1,8 +1,12 @@
 import { Layout, Menu, Breadcrumb } from 'antd';
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import '../../../../App.css';
-import { Card, Icon, Avatar } from 'antd';
-import { Col, Row } from 'antd';
+import { Card, Icon, Avatar, Col, Row } from 'antd';
+import game from "./game.jpg";
+import music from "./music.jpg";
+import book from "./book.jpg";
+
+
 const { Content, } = Layout;
 
 const { Meta } = Card;
@@ -10,81 +14,76 @@ const { Meta } = Card;
 export class Cards extends Component {
   render() {
     return (
-
       <Layout>
-          <Menu
-            theme="dark"
-            mode="horizontal"
-            defaultSelectedKeys={['2']}
-            style={{ textAlign: "center", lineHeight: '64px' }}
-          >
-            <Menu.Item key='2' style={{ fontSize: "32px" }}>Resources Page</Menu.Item>
+        <div style={{color:"white", padding: "10px 400px", textAlign: "center", lineHeight: '64px', fontSize: "40px" }}>
+          <Content style={{backgroundColor:"#000066"}}>
+            Resources Page
+          </Content>
+        </div>
 
-          </Menu>
-
-        <Content style={{ padding: "0px 75px", marginTop: 25, marginBottom: "50%" }}>
+        <Content style={{ padding: "0px 75px", marginTop: 25, marginBottom: "25%" }}>
           <Breadcrumb style={{ margin: '10px' }}>
             <Breadcrumb.Item>Home</Breadcrumb.Item>
             <Breadcrumb.Item>List</Breadcrumb.Item>
             <Breadcrumb.Item>App</Breadcrumb.Item>
           </Breadcrumb>
-          <div style={{ background: '#ECECEC', padding: '30px' }}>
-            <Row gutter={16}>
-              <Col span={8}>
-                <Card title="Card title" bordered={false}>Card content</Card>
-              </Col>
-              <Col span={8}>
-                <Card title="Card title" bordered={false}>Card content</Card>
-              </Col>
-              <Col span={8}>
-                <Card title="Card title" bordered={false}>Card content</Card>
-              </Col>
-            </Row>
-          </div>
 
           <div style={{ background: '#a0beef', padding: '30px' }}>
             <Row gutter={16}>
               <Col span={8}>
                 <Card
-                  style={{ width: 300 }}
-                  cover={<img alt="example" src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png" />}
+                  style={{}}
+                  cover={<img alt="example" src={game} height="175" />}
                   actions={[<Icon type="setting" />, <Icon type="edit" />, <Icon type="ellipsis" />]}
                 >
                   <Meta
                     avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
-                    title="Card title"
-                    description="This is the description"
+                    title="Gaming Room"
+                    description="Take a break with some innovating games"
                   />
                 </Card>
-                /</Col>
+              </Col>
 
               <Col span={8}>
                 <Card
-                  style={{ width: 300 }}
-                  cover={<img alt="example" src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png" />}
+                  cover={<img alt="example" src={book} height="175" />}
                   actions={[<Icon type="setting" />, <Icon type="edit" />, <Icon type="ellipsis" />]}
                 >
                   <Meta
                     avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
-                    title="Card title"
-                    description="This is the description"
+                    title="Books"
+                    description="Train your brain with some book reading"
                   />
                 </Card>
-                /</Col>
+              </Col>
 
               <Col span={8}>
                 <Card
-                  style={{ width: 300 }}
-                  cover={<img alt="example" src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png" />}
+                  cover={<img alt="example" src={music} height="175" />}
                   actions={[<Icon type="setting" />, <Icon type="edit" />, <Icon type="ellipsis" />]}
                 >
                   <Meta
                     avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
-                    title="Card title"
-                    description="This is the description"
+                    title="Media"
+                    description="Relax to some great musics of your choice"
+                    style={{height:"0px 42px"}}
                   />
                 </Card>
-                /</Col>
+              </Col>
+            </Row>
+          </div>
+
+          <div style={{ background: '#ECECEC', padding: '30px' }}>
+            <Row gutter={16}>
+              <Col span={8}>
+                <Card title="Card title" bordered={true}>Card1</Card>
+              </Col>
+              <Col span={8}>
+                <Card title="Card title" bordered={false}>Card content</Card>
+              </Col>
+              <Col span={8}>
+                <Card title="Card title" bordered={false}>Card content</Card>
+              </Col>
             </Row>
           </div>
 
