c0146fb17713b7a0c204aec6749d9a6b226fb1a2
b6i0l5l <38479828+b6i0l5l@users.noreply.github.com>
2019-04-02 17:27:00 -0700

    put the dashboard.js and dashboard.css
    update dashboard.js and dashboard.css

--- src/components/pages/dashboard/Dashboard.js
+++ src/components/pages/dashboard/Dashboard.js
@@ -1,16 +1,99 @@
 import React, { Component } from 'react';
-import { Typography } from 'antd';
+import { Auth, API } from "aws-amplify";
+import { Avatar, message, Input, List, Skeleton, Popconfirm, Icon, Typography, Button } from 'antd';
+import uuid from "uuid";
+import './Dashboard.css';
+import Feeds from '../feeds/Feeds';
 
 
 
 
 const { Title } = Typography;
 export class Dashboard extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      confirmLoading: false,
+      buttonLoading: false,
+      current_subfeed: 'General',
+      loading: true,
+      user: '',
+      posts: [],
+      component: 1,
+      title: '',
+      content: '',
+      visible: false
+    }
+
+    this.getPosts = this.getPosts.bind(this);
+    
+  }
+  
+  async componentDidMount(){
+    Auth.currentAuthenticatedUser({
+        bypassCache: false // Optional, By default is false. If set to true, this call will send a request to Cognito to get the latest user data
+    }).then(user => {
+      this.setState({user: user.attributes.email})
+    })
+    .catch(err => console.log(err));
+    this.getPosts();
+  }
+
+
+
+  async getPosts() {
+    this.setState({
+      posts: [],
+      loading: true
+    })
+
+    try {
+      const posts = await API.get("posts", "/posts/get-posts");
+      // this.setState({posts});
+      posts.body.map((post) => (
+        this.setState({
+          posts: [
+            ...this.state.posts,
+            {
+              subfeed: post.subfeed,
+              timestamp: post.timestamp,
+              id: post.id,
+              user: post.user,
+              title: post.title,
+              content: post.content
+            }
+          ]
+        })
+      ));
+      // console.log(posts.body);
+      message.success('Successfully retrieved posts!');
+      this.setState({loading: false});
+    } catch (e) {
+      console.log(e);
+      this.setState({loading: false});
+    }
+  }
+  
   render() {
+    const data =this.state.posts
     return (
       <div>
-        <Title>Dashboard</Title>
+      <div class="grid-head">
+        <div>Hello!</div>
       </div>
+      <div class="grid-container">
+        <div><Feeds/>
+        
+
+        </div>
+        
+        <div>Resources</div>
+        <div>StudyGroup</div>
+      </div>
+      
+    </div>
+
     )
   }
 }
