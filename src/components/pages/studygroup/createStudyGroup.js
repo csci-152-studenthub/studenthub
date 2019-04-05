d5100181dbc601efa4f7e6884002afd8ff3654f5
Steven Ryan Ramos <43050949+rsteven46@users.noreply.github.com>
2019-04-05 09:36:29 -0700

    Update createStudyGroup.js
    fixed code added group members to creating group

--- src/components/pages/studygroup/createStudyGroup.js
+++ src/components/pages/studygroup/createStudyGroup.js
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'antd/dist/antd.css';
@@ -6,10 +7,35 @@ import {
   Form, Input, Tooltip, Icon, Cascader, Select, Row, Col, Checkbox, Button, AutoComplete,
 } from 'antd';
 
-const { Option } = Select;
-const AutoCompleteOption = AutoComplete.Option;
+
+
+let id = 0;
+
 
 class RegistrationForm extends React.Component {
+  remove = (k) => {
+    const { form } = this.props;
+    // can use data-binding to get
+    const keys = form.getFieldValue('keys');
+    // We need at least one passenger
+    if (keys.length === 1) {
+      return;
+    }
+    form.setFieldsValue({
+      keys: keys.filter(key => key !== k),
+    });
+  }
+  add = () => {
+    const { form } = this.props;
+    // can use data-binding to get
+    const keys = form.getFieldValue('keys');
+    const nextKeys = keys.concat(id++);
+    // can use data-binding to set
+    // important! notify form to detect changes
+    form.setFieldsValue({
+      keys: nextKeys,
+    });
+  }
   state = {
     confirmDirty: false,
     autoCompleteResult: [],
@@ -19,33 +45,17 @@ class RegistrationForm extends React.Component {
     e.preventDefault();
     this.props.form.validateFieldsAndScroll((err, values) => {
       if (!err) {
+        const { keys, names } = values;
+        console.log('Received values of form: ', values);
+        console.log('Merged values:', keys.map(key => names[key]));
         console.log('Received values of form: ', values);
       }
     });
   }
 
-  validateToNextPassword = (rule, value, callback) => {
-    const form = this.props.form;
-    if (value && this.state.confirmDirty) {
-      form.validateFields(['confirm'], { force: true });
-    }
-    callback();
-  }
-
-  handleWebsiteChange = (value) => {
-    let autoCompleteResult;
-    if (!value) {
-      autoCompleteResult = [];
-    } else {
-      autoCompleteResult = ['.com', '.org', '.net'].map(domain => `${value}${domain}`);
-    }
-    this.setState({ autoCompleteResult });
-  }
 
   render() {
-    const { getFieldDecorator } = this.props.form;
-    const { autoCompleteResult } = this.state;
-
+    const { getFieldDecorator, getFieldValue } = this.props.form;
     const formItemLayout = {
       labelCol: {
         xs: { span: 24 },
@@ -56,6 +66,13 @@ class RegistrationForm extends React.Component {
         sm: { span: 16 },
       },
     };
+    const formItemLayoutWithOutLabel = {
+      wrapperCol: {
+        xs: { span: 24, offset: 0 },
+        sm: { span: 20, offset: 8 },
+      },
+    };
+    
     const tailFormItemLayout = {
       wrapperCol: {
         xs: {
@@ -63,19 +80,48 @@ class RegistrationForm extends React.Component {
           offset: 0,
         },
         sm: {
-          span: 16,
+          span: 20,
           offset: 8,
         },
       },
     };
     
 
-    const websiteOptions = autoCompleteResult.map(website => (
-      <AutoCompleteOption key={website}>{website}</AutoCompleteOption>
+    getFieldDecorator('keys', { initialValue: [] });
+    const keys = getFieldValue('keys');
+    const formItems = keys.map((k, index) => (
+      <Form.Item
+        {...(index === 0 ? formItemLayout : formItemLayoutWithOutLabel)}
+        label={index === 0 ? 'Members E-mail' : ''}
+        required={false}
+        key={k}
+      >
+        {getFieldDecorator(`names[${k}]`, {
+          validateTrigger: ['onChange', 'onBlur'],
+          rules: [{
+            required: true,
+            whitespace: true,
+            message: "Please input members email or delete this field.",
+          },
+          { pattern: '^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.+-]+\.edu$', message: 'Only edu emails can be accepted!'},
+          { type: 'email', message: 'This is not a valid E-mail!'},
+          { required: true, message: 'Please input your E-mail!'}],
+        })(
+          <Input placeholder="Members E-mail" style={{ width: '60%', marginRight: 8 }} />
+        )}
+        {keys.length > 1 ? (
+          <Icon
+            className="dynamic-delete-button"
+            type="minus-circle-o"
+            disabled={keys.length === 1}
+            onClick={() => this.remove(k)}
+          />
+        ) : null}
+      </Form.Item>
     ));
-
     return (
       <Form {...formItemLayout} onSubmit={this.handleSubmit}>
+        
         <Form.Item
           label="Study Group Name"
         >
@@ -105,17 +151,19 @@ class RegistrationForm extends React.Component {
         >
           {getFieldDecorator('professor', {
             rules: [{
-              required: true, message: 'Please confirm your password!',
-            }, {
-              validator: this.compareToFirstPassword,
+              required: true, message: 'Please input a professor for the course!',
             }],
           })(
             <Input placeholder="Dr Liu" />
           )}
         </Form.Item>
-
-       
+        {formItems}
         
+        <Form.Item {...formItemLayoutWithOutLabel}>
+          <Button type="dashed" onClick={this.add} style={{ width: '60%' }}>
+            <Icon type="plus" /> Add study group members
+          </Button>
+        </Form.Item>
         <Form.Item {...tailFormItemLayout}>
           <Button type="primary" htmlType="submit">Create the group</Button>
         </Form.Item>
