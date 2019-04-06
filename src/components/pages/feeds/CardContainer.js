216721cd7aa9f52f8ff5941b6c5ab1c7f84d13a3
Nina <swaavayyy@mail.fresnostate.edu>
2019-04-06 15:03:54 -0700

    added api for resources

--- src/components/pages/feeds/CardContainer.js
+++ src/components/pages/feeds/CardContainer.js
@@ -68,7 +68,6 @@ export class CardContainer extends Component {
     Auth.currentAuthenticatedUser({
         bypassCache: true  // Optional, By default is false. If set to true, this call will send a request to Cognito to get the latest user data
     }).then(user => {
-
       this.props.setHeader('General');
       this.setState({user: user.attributes.preferred_username})
     })
--- src/components/pages/resources/resComp/Cards.js
+++ src/components/pages/resources/resComp/Cards.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import '../../../../App.css';
-import { Card, Icon, Avatar, List, } from 'antd';
+import { Card, List, Button } from 'antd';
+import { API, Auth } from "aws-amplify";
+import uuid from "uuid";
+
 import game from "./game.jpg";
-import music from "./music.jpg";
-import book from "./book.jpg";
 import booki from "./book1.png"
 import pikachu from "./pikachu.png";
 import pac from "./pac1.jpg";
@@ -18,50 +19,65 @@ export class Cards extends Component {
   constructor(){
     super();
     this.state = {
-      data: [
-        {
-          title: 'Gamer',
-          url: game,
-          image: pikachu,
-          description: 'Take a break with some innovating games',
-        },
-        {
-          title: 'Title 2',
-          url: game,
-          image: booki,
-          description: 'Take a break with some innovating games',
-        },
-        {
-          title: 'Title 3',
-          url: game,
-          image: pac,
-          description: 'Take a break with some innovating games',
-        },
-      ]
+      data: [],
+      user:''
     }
   }
+  
+  async componentDidMount(){
+    Auth.currentAuthenticatedUser({
+      bypassCache:true
+    }).then(user =>{
+      this.setState({user: user.attributes.preferred_username});
+    })
+    .catch(error => console.log(error));
+  }
+
+  async createResources(){
+    let resourceId = 'resource-'+uuid.v4().toString();
+    let resource_title = "this.state.title";
+    let resource_description = "this.state.description";
+    let created_by = this.state.user;
+    let timestamp = new Date().toLocaleString();
+
+    let apiName = 'posts';
+    let path = '/resources/create-resource';
+    let myInit = {
+      body: {resourceId, resource_title, resource_description, created_by, timestamp}
+    }
+    await API.post(apiName, path, myInit).then(response => {
+      console.log("Resource post sent", response);
+     
+    }).catch(error => {
+      console.log(error);
+    })
+  }
+
   render() {
     return (
-      <List
-        grid={{ gutter: 16, column: 4 }}
-        dataSource={this.state.data}
-        renderItem={item => (
-          <List.Item>
-            <Card
-              hoverable
-              //title={item.title}
-              cover={<img src={item.url} />}
-              actions={[<Icon type="setting" />, <Icon type="edit" />]}
-            >
-              <Meta
-                // avatar={<Avatar src={item.image} />}
-                title={item.title}
-                description={item.description}
-              />
-            </Card>
-          </List.Item>
-        )}
-      />
+      <div>
+        <Button onClick={() => this.createResources() }>Create Resource</Button>
+        <List
+          grid={{ gutter: 16, column: 4 }}
+          dataSource={this.state.data}
+          renderItem={item => (
+            <List.Item>
+              <Card
+                hoverable
+                //title={item.title}
+                cover={<img src={item.url} />}
+                // actions={[<Icon type="setting" />, <Icon type="edit" />]}
+              >
+                <Meta
+                  // avatar={<Avatar src={item.image} />}
+                  title={item.title}
+                  description={item.description}
+                />
+              </Card>
+            </List.Item>
+          )}
+        />
+      </div>
     )
   }
 }
