7486b708b4c0e87f470fee53a84fecc662c21515
b6i0l5l <b6i0l5l@mail.fresnosate.edu>
2020-09-16 16:08:12 +0800

    feat(api): add api function and create Postapi folder

--- src/api/dashboard_api.js/get_api.js
+++ src/api/dashboard_api.js/get_api.js
@@ -3,7 +3,11 @@ import { API } from "aws-amplify";
 
 export default {
     getposts(){
-        const getposts = API.get("posts", "/posts/get-posts")
+        const getposts = API.get("posts", "/posts/get-posts");
         return getposts;
+    },
+    getresources(useremail){
+        const getresources = API.get("posts", "/resources/get-resources", useremail);
+        return getresources;
     }
 };
