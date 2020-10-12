72f977ee05134064351ab733ddd26a5fec357136
b6i0l5l <b6i0l5l@mail.fresnosate.edu>
2020-10-12 15:45:40 +0800

    feat(dashboard): By using useEffect and useState to replace ComponentDidMount and this.State. Add DashboardInit.js to init the dashboard state.

--- src/components/pages/dashboard/Dashboard.js
+++ src/components/pages/dashboard/Dashboard.js
@@ -4,64 +4,100 @@ import {List, Typography, Skeleton, Divider, Statistic, Row, Col, Icon, Spin} fr
 import GetApi from '../../../api/dashboard_api.js/get_api.js';
 import PostApi from '../../../api/dashboard_api.js/post_api.js';
 import './Dashboard.css';
+import Init from './DashboardInit';
 
 const { Text ,Title } = Typography;
 
 const Dashboard = () => {
-  const[count,setCount] = useState(0);
-  const[{userAttributes,content,study_groups},setState]=useState({userAttributes:"",content:"MEMe",study_groups:[]}) ;
-  useEffect(async ()=>{
-     await Auth.currentAuthenticatedUser({bypassCache: true}).then(response => {
-      // console.log('Setting userAttributes to:', response.attributes);
-      setState({userAttributes: response.attributes})
-    }).catch(err => console.log(err));
-      // console.log(data.attributes);
-    
-    getStudygroups();
-    
+  const[{
+    confirmLoading,
+    buttonLoading,
+    current_subfeed,
+    loading, 
+    user, 
+    component, 
+    title,
+    content,
+    visible
+  }] = useState(Init);
+    const [userattributes, setAttributes] = useState(undefined);
+    const [study_groups, setStudyGroups] = useState(undefined);
+    const [posts, setPosts] = useState(undefined);
+    const [resources, setResources] = useState(undefined);
+    const [statistics, setStatistics] = useState(undefined);
+
+  useEffect(()=>{
+    const auth = async () => {
+      await Auth.currentAuthenticatedUser({
+        bypassCache: true  // Optional, By default is false. If set to true, this call will send a request to Cognito to get the latest user data
+      }).then(response => {
+        // console.log('Setting userAttributes to:', response.attributes);
+        setAttributes(response.attributes);
+        getPosts();
+        getResources(response.attributes);
+        getStudygroups(response.attributes);
+        getStatistics(response.attributes);
+      }).catch(err => console.log(err));
+    }
+    // this.props.handler("Dashboard");
+    auth();
   },[]);
 
-  async function getStudygroups(){
+  async function getPosts(){
     try{
-        const useremail = {userAttributes}['userAttributes']['email'];
+        const posts = await GetApi.getposts();
+        console.log(posts);
+        setPosts(posts.body);
+    }catch(e){
+      console.log('error',e);
+    }
+  }
+
+  async function getResources(userattributes){
+    let email = userattributes.email;
+    let myInit = {
+      body: {user: email}
+    };
+    const resources = await GetApi.getresources(myInit);
+    setResources(resources.body);
+  }
+
+  async function getStudygroups(userAttributes){
+    try{
+        let user = userAttributes.email;
         let myInit = {
-          body: {user:useremail}
+          body: {user}
         };
         const studygroups = await PostApi.getstudygroups(myInit);
-        console.log(studygroups);
-        studygroups.body.map((item) => setState(state=>({
-          study_groups:[
-            {
-              ...state,
-              groupId: item.groupId,
-              course: item.course,
-              group_name: item.group_name,
-              description: item.description,
-              members: item.members,
-              professor: item.professor,
-              timestamp: item.timestamp,
-              created_by: item.created_by
-            }
-          ]
-        })))
-        setState({cardsLoading: false});
+        setStudyGroups(studygroups.body);
     }catch(e){
       console.log("error",e);
-      
     }
     
   }
   
-  
+  async function getStatistics(userAttributes){
+    try{
+        let user = userAttributes.preferred_username;
+        let userEmail = userAttributes.email;
+        let myInit = {
+        body: {user, userEmail}
+        };
+        const statistic = await PostApi.getstatistics(myInit);
+        setStatistics(statistic.body);
+      }catch(e){
+        console.log("error",e);
+      }
+  }
 
   
 
   
-  // const post_data =this.state.posts;
-  // const resource_data = this.state.resources;
-  // const studygroup_data = this.state.study_groups;
-
-  // let userAttributes = this.state.userAttributes;
+  const post_data = posts; 
+  const resource_data = resources;
+  const studygroup_data = study_groups;
+  
+  let userAttributes = userattributes;
   if(userAttributes === undefined)
     return (<Skeleton />);
   else
@@ -70,30 +106,29 @@ const Dashboard = () => {
         <div className="item-welcome-board">
           <div className="welcome-message-box">
             <div className="typewriter">
-              {/* <button onClick = {Toggle}>Click me!</button> */}
-              <Title level={3}>Hello {userAttributes['email']}</Title>
-              {/* <Title level={3}>Hello {userAttributes.name}</Title> */}
+
+              <Title level={3}>Hello {userAttributes?userAttributes.name:null}</Title>
             </div>
           </div>
         
           <div className="item-statistics">
             <div className="item-feed-stats">
-              {/* <Statistic title="Total Posts" value={this.state.statistics ? this.state.statistics.postCount : "😪"} prefix={this.state.statistics ? <Icon type="project" rotate={-90}  /> : <Spin />} />
-              <Statistic title="Total Likes" value={this.state.statistics ? this.state.statistics.postLikeCount : "😪"} prefix={this.state.statistics ? <Icon type="like" /> : <Spin />} /> */}
+              <Statistic title="Total Posts" value={statistics ? statistics.postCount : "😪"} prefix={statistics ? <Icon type="project" rotate={-90}  /> : <Spin />} />
+              <Statistic title="Total Likes" value={statistics ? statistics.postLikeCount : "😪"} prefix={statistics ? <Icon type="like" /> : <Spin />} />
             </div>
             <div className="item-resource-stats">
-              {/* <Statistic title="Total Resources" value={this.state.statistics ? this.state.statistics.resourceCount : "😪"} prefix={this.state.statistics ? <Icon type="read" /> : <Spin />} />
-              <Statistic title="Resource Views" value={this.state.statistics ? this.state.statistics.resourceViewCount : "😪"} prefix={this.state.statistics ? <Icon type="eye" /> : <Spin />} /> */}
+              <Statistic title="Total Resources" value={statistics ? statistics.resourceCount : "😪"} prefix={statistics ? <Icon type="read" /> : <Spin />} />
+              <Statistic title="Resource Views" value={statistics ? statistics.resourceViewCount : "😪"} prefix={statistics ? <Icon type="eye" /> : <Spin />} />
             </div>
           </div>
 
         </div>
         <div className="item-activity-board">
-          {/* <Divider className="item-feed-title"><Text style={{fontSize: 32}}>Feeds</Text></Divider> */}
+          <Divider className="item-feed-title"><Text style={{fontSize: 32}}>Feeds</Text></Divider>
           <div className="item-feed-activity">
-            {/* <List
+            <List
               itemLayout="horizontal"
-              dataSource={post_data}
+              dataSource={post_data? post_data:[]}
               renderItem={item => (
                 <List.Item>
                   <List.Item.Meta
@@ -101,13 +136,13 @@ const Dashboard = () => {
                   />
                 </List.Item>
               )}
-            /> */}
+            />
           </div>
-          {/* <Divider className="item-resource-title"><Text style={{fontSize:32}}>Resources</Text></Divider> */}
+          <Divider className="item-resource-title"><Text style={{fontSize:32}}>Resources</Text></Divider>
           <div className="item-resources-activity">
-            {/* <List
+            <List
                 itemLayout="horizontal"
-                dataSource={resource_data}
+                dataSource={resource_data? resource_data:[]}
                 renderItem={item => (
                   <List.Item>
                     <List.Item.Meta
@@ -120,13 +155,13 @@ const Dashboard = () => {
                     />
                   </List.Item>
                 )}
-            /> */}
+            />
           </div>
           <Divider className="item-studygroup-title"><Text style={{fontSize: 32}}>Study Groups</Text></Divider>
           <div className="item-studygroups-activity">
-              {/* <List
+              <List
                 itemLayout="horizontal"
-                dataSource={studygroup_data}
+                dataSource={studygroup_data? study_groups:[]}
                 renderItem={item => (
                   <List.Item>
                     <List.Item.Meta
@@ -139,7 +174,7 @@ const Dashboard = () => {
                     />
                   </List.Item>
                 )}
-            /> */}
+            />
           </div>
         </div>
       </div>
@@ -148,4 +183,3 @@ const Dashboard = () => {
 }
 
 export default Dashboard
-
